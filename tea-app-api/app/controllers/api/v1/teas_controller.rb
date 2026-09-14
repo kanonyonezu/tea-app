fc8733e5a7f032bdf9d6b957f62d49aa080de596
Gifta Santosa <giftaputri.santosa@gmail.com>
2026-09-15 00:01:17 +0900

    Halfway done with POST request

---
+++ tea-app-api/app/controllers/api/v1/teas_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::TeasController < ApplicationController
+  def index
+    @teas = Tea.all
+
+    render json: @teas.order(name_en: :asc)
+  end
+
+  def recommendation
+    @tea_request = TeaRequest.new(tea_params)
+    if @tea_request.save
+      render json: @tea_request, status: :created
+    else
+      render json: { error: @tea_request.errors.messages}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def tea_params
+    params.require(:preference).permit(:category, :flavor)
+  end
+end
