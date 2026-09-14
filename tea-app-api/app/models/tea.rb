70d21a99e45ec8bfb582963b3872edaa54bbcd6b
Gifta Santosa <giftaputri.santosa@gmail.com>
2026-09-14 23:24:27 +0900

    Added seeds

--- tea-app-api/app/models/tea.rb
+++ tea-app-api/app/models/tea.rb
@@ -2,4 +2,6 @@ class Tea < ApplicationRecord
   has_many :tea_requests
 
   validates :name_en, presence: true, uniqueness:true
+  normalizes :category, with: ->(values) { values.map{ |values| values.strip}.uniq }
+
 end
